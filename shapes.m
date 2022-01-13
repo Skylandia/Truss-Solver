9004cbddbca915f5daccc733105e2765c17c8762
Donut-the-1st <93704931+Donut-the-1st@users.noreply.github.com>
2022-01-14 12:39:25 +1300

    Added shape class

---
+++ shapes.m
@@ -0,0 +1,9 @@
+classdef shapes
+    %SHAPES An Enumeration for shapes
+    %   Allows for a shape to be a propertie of a dead zone or a load zone
+    %   instead of a cell array
+    enumeration
+        rectangle, circle, triangle
+    end
+end
+
