02ddf30310dd78b689c5a336fc4be63b92f324ba
Donut-the-1st <93704931+Donut-the-1st@users.noreply.github.com>
2022-02-10 22:21:35 +1300

    hope

--- ConfigFileMaker.py
+++
@@ -1,8 +0,0 @@
-import configparser
-
-
-config = configparser.ConfigParser()
-config['Discord'] = {}
-config['Discord']['Bot Key'] = 'OTQwMTUwMzg0MzQ3OTkyMDc0.YgDNRg.VVcbQxG5uM-POp8n8YB3ZPnRfpY'
-with open('config.ini', 'w') as configfile:
-    config.write(configfile)
