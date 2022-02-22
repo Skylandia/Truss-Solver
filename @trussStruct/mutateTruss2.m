9cfcb8c043fb977217fc26a37f9e2225bacc044d
Donut-the-1st <93704931+Donut-the-1st@users.noreply.github.com>
2022-02-23 10:36:23 +1300

    Fixed bug where mutateTruss2 could just get stuck

--- @trussStruct/mutateTruss2.m
+++ @trussStruct/mutateTruss2.m
@@ -1,13 +1,14 @@
 function obj = mutateTruss2(obj, loadZone, restrictedZoneArray)
 
 valid = false;
+connectionsMatrix = obj.endNodes;
 while ~valid
     nodeLocations = [[obj.nodesArray.x]',[obj.nodesArray.y]'] + [[0,0];normrnd(0,0.00025, obj.numNodes-2, 2);[0,0]];
-    connectionsMatrix = obj.endNodes;
-    obj = generateTrussStruct(nodeLocations, connectionsMatrix, obj.weightNode);
-    valid = obj.validateEdgeLengths(0.150) &&...
-        loadZone.isWeightNodeInZone(obj)...
-        && all(cellfun(@(panic) isRestricted(panic,obj), restrictedZoneArray));
+    tempObj = generateTrussStruct(nodeLocations, connectionsMatrix, obj.weightNode);
+    valid = tempObj.validateEdgeLengths(0.150) &&...
+        loadZone.isWeightNodeInZone(tempObj)...
+        && all(cellfun(@(panic) isRestricted(panic,tempObj), restrictedZoneArray));
 end
+obj = tempObj;
 end
 
