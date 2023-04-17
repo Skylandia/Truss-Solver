7e3f118de1ee4a9d89d72f24e963478f404134d4
Skylandia <xrealify@gmail.com>
2023-04-18 04:36:19 +1200

    Update truss sorting algorithm to sort based on load/cost ratio

--- sortTrussArray.m
+++ sortTrussArray.m
@@ -2,15 +2,11 @@ function [trussArray] = sortTrussArray(trussArray)
 %SORTTRUSSARRAY Summary of this function goes here
 %   Detailed explanation goes here
 %sorts
-if ~iscell(trussArray)
-    [~, indexes] = sort([trussArray.cost]);
-    trussArray = trussArray(indexes);
-    [~, indexes] = sort([trussArray.capasity], 'descend');
+if iscell(trussArray)
+    [~, indexes] = sort(cellfun(@(truss) truss.capasity / truss.cost, trussArray), 'descend');
     trussArray = trussArray(indexes);
 else
-    [~, indexes] = sort(cellfun(@(ahhhhh) ahhhhh.cost, trussArray));
-    trussArray = trussArray(indexes);
-    [~, indexes] = sort(cellfun(@(ahhhhh) ahhhhh.capasity, trussArray),'descend');
+    [~, indexes] = sort(trussArray.capasity ./ trussArray.cost, 'descend');
     trussArray = trussArray(indexes);
 end
 end
